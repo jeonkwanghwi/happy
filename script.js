10f5c8d4d8ec0b0997e3f6b36a6a1e8431ead736
kwanghwi <nimo6289@gmail.com>
2024-07-30 23:31:40 +0900

    로컬 스토리지

--- script.js
+++ script.js
@@ -1,9 +1,24 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Load tasks from localStorage
+    loadTask('yeba');
+    loadTask('panghui');
+});
+
 function registerTask(user) {
     let inputId = user + '-input';
     let taskId = user + '-task';
     let inputValue = document.getElementById(inputId).value;
     if (inputValue.trim() !== "") {
         document.getElementById(taskId).innerText = inputValue;
+        localStorage.setItem(taskId, inputValue);
         document.getElementById(inputId).value = "";
+    }
+}
+
+function loadTask(user) {
+    let taskId = user + '-task';
+    let savedTask = localStorage.getItem(taskId);
+    if (savedTask) {
+        document.getElementById(taskId).innerText = savedTask;
     }
 }
