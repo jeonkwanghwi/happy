271bfce1e46c336bfa32b6d1be09d69cd92176e2
KwangHwi Jeon <51524745+jeonkwanghwi@users.noreply.github.com>
2024-07-30 23:18:47 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function registerTask(user) {
+    let inputId = user + '-input';
+    let taskId = user + '-task';
+    let inputValue = document.getElementById(inputId).value;
+    if (inputValue.trim() !== "") {
+        document.getElementById(taskId).innerText = inputValue;
+    }
+}
